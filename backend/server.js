94c402c74e72ed8c646a4cd22e0e10912b300d05
Alexandr Kuznetsov <47721260+AlexSKuznetsov@users.noreply.github.com>
2023-05-02 22:41:46 +0300

    Add webapp routing, add rest api for gathering processes list

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
-
+const cors = require('cors');
+const axios = require('axios');
 const app = express();
+require('dotenv').config()
 
 const PORT = 5000;
 
+if (!process.env.CAMUNDA_API_BASE_URL) {
+  throw new Error('Camunda api url not found!')
+}
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+
+app.use(cors());
+
+app.get('/', async (req, res) => {
+  try {
+    const response = await axios.get(`${process.env.CAMUNDA_API_BASE_URL}/deployment`)
+    res.send(response.data)
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 app.listen(PORT, () => {
